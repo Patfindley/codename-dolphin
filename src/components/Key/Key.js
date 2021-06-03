a00e945cb4ef5ae0f2691404de372ff47e429f00
Chris Anderson <mistercanderson@gmail.com>
2021-06-02 18:38:36 -0600

    Refactor Keyboard

--- src/components/Key/Key.js
+++ src/components/Key/Key.js
@@ -1,15 +1,13 @@
-import './Key.css'
+import React, { useState } from 'react';
+import './Key.css';
 
-const Key = ({ note, index, pressedKeys }) => {
-
-    return (
-        <div className="key">
-            <div className="note-display">
-                { note.toUpperCase() }
-            </div>
-        </div>
-    )
-
-}
+const Key = ({ note, index, activateKey, isSharp }) => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div className={isSharp ? 'key sharp' : 'key'}>
+      <div className='note-display'>{note.toUpperCase()}</div>
+    </div>
+  );
+};
 
 export default Key;
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,39 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import Key from '../Key/Key.js'
-import './Keyboard.css'
-import '../../util/keyboardSwitch'
+import Key from '../Key/Key.js';
+import './Keyboard.css';
+import '../../util/keyboardSwitch';
 
 const Keyboard = (props) => {
-  const [pressedKeys, setPressedKeys] = useState([])
-  const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F']
+  const [activeKeys, setActiveKeys] = useState([]);
+  const notes = [
+    'C',
+    'C#',
+    'D',
+    'D#',
+    'E',
+    'F',
+    'F#',
+    'G',
+    'G#',
+    'A',
+    'A#',
+    'B',
+    'C',
+    'C#',
+    'D',
+    'D#',
+    'E',
+    'F',
+  ];
   const noteIsSharp = (note) => {
-      return note.includes('#')
-  }
+    return note.includes('#');
+  };
 
-  return (
-  <div className="keyboard">
-    {notes.map((note, index) => {
-      let keyClassName = "key"
-       if (noteIsSharp(note)) {
-           keyClassName += " sharp"
-        }
-        if (noteIsSharp(note)) {
-            return (
-              <div className={keyClassName}>
-              </div>    
-            )   
-        }
+  const activateKey = (key) => {
+    //if note === key.note
+    //keyClassName += " active"
+  };
 
+  const mapNotes = (notes) => {
+    return notes.map((note, index) => {
+      if (noteIsSharp(note)) {
+        console.log(note, 'sharp')
         return (
-          <div className={keyClassName}>
-             <Key 
-                key={index}
-                note={note}
-                pressedKeys={pressedKeys} />
-          </div>
-    )})}
-</div>
-  )
-}
+          <Key
+            key={index}
+            note={note}
+            activateKey={activateKey}
+            isSharp={true}
+          /> 
+        );
+      } else {
+        return (
+          <Key
+            key={index}
+            note={note}
+            activateKey={activateKey}
+          />
+        );
+      }
+    });
+  };
+
+  return <div className='keyboard'>{mapNotes(notes)}</div>;
+};
 
 export default Keyboard;
