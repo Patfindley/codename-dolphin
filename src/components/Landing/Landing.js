de9cb6f5ade13e856147b3ec9035008249fa3f03
Patrick <>
2021-06-04 15:38:34 -0400

    Import dolphin to landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,26 @@
+import DelayLink from '../DelayLink/DelayLink.js'
+import Dolphin from '../Dolphin/Dolphin';
+import { gsap } from 'gsap';
+import dolphinImg from '../../assets/dolphin.svg';
 import './Landing.css'
 
 const Landing = () => {
+
+  // var tl = new Timelinelite({repeat:-1})
+  const clickAnimation = () => {
+    gsap.to(".dolphin-arm", 5, {rotation:-360, transformOrigin:"left"})
+    gsap.to(".dolphin-title", 5, {rotation:200, visibility: "visible"}, 0)
+  }
+
   return (
     <div className="landing-container">
-      <h1 className="landing-title"> Fuck It Up </h1>
+      <DelayLink delay={1500}
+      to="/synth">
+        <h1 className="landing-title" onClick={clickAnimation}> Fuck It Up </h1>
+        </DelayLink>
+      <div className="dolphin-arm">
+        <img className="dolphin-title" src={dolphinImg} alt="dolphin" />
+      </div>
     </div>
   )
 }
