a90cb9fb7f4f0d70d6953eb872ffd2ab74702df0
Patrick <>
2021-06-03 22:13:17 -0400

    Add dolphin animation component

---
+++ src/components/Dolphin/Dolphin.js
@@ -0,0 +1,21 @@
+import {gsap, TweenLite, TimelineMax} from 'gsap';
+import dolphinImg from '../../assets/dolphin.svg';
+import './Dolphin.css';
+
+const Dolphin = () => {
+
+  // TweenLite.defaultEase = Linear.easeNone;
+
+  var tl = new TimelineMax({repeat:-1})
+
+  tl.to(".dolphin-container", 5, {rotation:-360, transformOrigin:"left"})
+ .to(".dolphin", 5, {rotation:200}, 0)
+
+  return (
+    <div className="dolphin-container">
+    <img className="dolphin" src={dolphinImg} alt="dolphin"/>
+    </div>
+  )
+}
+
+export default Dolphin;
