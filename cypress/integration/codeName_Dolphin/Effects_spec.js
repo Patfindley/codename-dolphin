0dd256c4e4253f3e0d2f20eb8ae291f1e3207c5f
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-06-06 12:09:58 -0600

    Add Effects spec integration tests

--- cypress/integration/codeName_Dolphin/Effects_spec.js
+++ cypress/integration/codeName_Dolphin/Effects_spec.js
@@ -5,11 +5,18 @@ describe('Effects section', () => {
     cy.viewport(1920, 975) 
   });
 
-  it('should allow the user to adjust effects', () => {
+  it('should allow the user to adjust filter effects', () => {
     cy.visit('/synth');
     cy.get('[name="lpfilter"]:input').as('filter');
     cy.get('@filter').should('have.value', 1200).invoke('val', 8000).trigger('change')
     cy.get('@filter').should('have.value', 8000)
+  });
+
+  it('should allow the user to adjust volume effects', () => {
+    cy.visit('/synth');
+    cy.get('[name="volume"]:input').as('volume');
+    cy.get('@volume').should('have.value', -20).invoke('val', -9).trigger('change')
+    cy.get('@volume').should('have.value', -9)
   });
 
 });
