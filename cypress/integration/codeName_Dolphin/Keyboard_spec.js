157fba70e432abb8aa7edc1c86e42e0f939faca4
Patrick <>
2021-06-05 21:27:49 -0400

    Test piano roll click

--- cypress/integration/codeName_Dolphin/Keyboard_spec.js
+++ cypress/integration/codeName_Dolphin/Keyboard_spec.js
@@ -18,6 +18,13 @@ describe('Landing', () => {
     .should('have.css', 'background-color', 'rgb(255, 0, 0)' )
   })
 
+  it('Should respond to a click or touch', () => {
+    cy.get('[note="D#4"]')
+    .should('have.css', 'background-color', 'rgb(50, 58, 158)')
+    cy.get('[note="D#4"]').click()
+    .should('have.css', 'background-color', 'rgb(255, 0, 0)' )
+  })
+
   
  
 })
