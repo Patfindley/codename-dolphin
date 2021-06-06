a9f9b1135edc93147ce488bbaf35bf25bcc92898
Patrick <>
2021-06-05 21:27:14 -0400

    Test key press

--- cypress/integration/codeName_Dolphin/Keyboard_spec.js
+++ cypress/integration/codeName_Dolphin/Keyboard_spec.js
@@ -10,6 +10,14 @@ describe('Landing', () => {
     cy.url().should('include', '/synth')
   })
 
+  it('Should respond to keyboard press', () => {
+    cy.get('[note=F4]')
+    .should('have.css', 'background-color', 'rgba(255, 255, 255, 0.847)')
+    .get('[note=F4]')
+    .trigger('keydown', { keyCode: 70})
+    .should('have.css', 'background-color', 'rgb(255, 0, 0)' )
+  })
+
   
  
 })
